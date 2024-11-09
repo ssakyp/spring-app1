33108afaca10c39ffe52e46c670baa8fdc7ed0cb
ssakyp <sultansakyp77@gmail.com>
2024-11-09 12:06:47 +0500

    @Qualifier

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -9,16 +10,24 @@ import java.util.List;
 @Component
 public class MusicPlayer {
     //private List<Music> musicList = new ArrayList<>();
-    //@Autowired
-    //private Music music;
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+//    @Autowired
+//    @Qualifier("someRockMusic")
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic){
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("someRockMusic")Music music1, @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
+    //    private ClassicalMusic classicalMusic;
+//    private RockMusic rockMusic;
+
+//    @Autowired
+//    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic){
+//        this.classicalMusic = classicalMusic;
+//        this.rockMusic = rockMusic;
+//    }
 
     private String name;
     private String volume;
@@ -50,8 +59,8 @@ public class MusicPlayer {
 //    }
 
     public String playMusic(){
-        System.out.println("Playing " + rockMusic.getSong());
-        return "Playing " + classicalMusic.getSong();
+        System.out.println();
+        return "Playing " + music1.getSong() + ", \n" + music2.getSong();
     }
 
 
